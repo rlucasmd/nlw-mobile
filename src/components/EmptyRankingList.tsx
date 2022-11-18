4f11b3c82fc4f0aac908e0cd8e0b176e3d9a770a
Ranieri Lucas <ranieri.dantas.122@ufrn.edu.br>
2022-11-18 14:40:15 -0300

    add: game card component

---
+++ src/components/EmptyRankingList.tsx
@@ -0,0 +1,11 @@
+import { Text } from 'native-base';
+function EmptyRankingList() {
+  return (
+    <Text color="white" fontSize="sm" textAlign="center">
+      O ranking desse bolão ainda não foi {'\n'}
+      formado, aguarde os resultados.
+    </Text>
+  );
+}
+
+export { EmptyRankingList }
